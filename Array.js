5179c998be9567430b7e6da5a7508b804ef002bc
CodeWithkaran02 <7870karuna@gmail.com>
2023-03-21 00:09:01 +0530

    Array

---
+++ Array.js
@@ -0,0 +1,14 @@
+//Empty element 
+let arr = [];
+console.log(arr);
+
+//array with element 
+let eleArr = [1, 2, 3, 4, 5, "hello World I am String ", false, 'e', 4.5];
+console.log(eleArr);
+
+// check how know is where is my 5 number
+console.log("Element at 4th index:" + eleArr[4]);
+
+//Replce the element 
+eleArr[3] = "Nothing ";
+console.log(eleArr);
