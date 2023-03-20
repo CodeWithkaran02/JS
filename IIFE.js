543f6220a6fff96329150b7687a6e9ffebc3b991
CodeWithkaran02 <7870karuna@gmail.com>
2023-03-20 23:53:47 +0530

    IIFS commit

---
+++ IIFE.js
@@ -0,0 +1,11 @@
+//IIES -> It is a IMMEDUATEKY INVOKED FUNCTION EXPRESSION 
+//Without parameter
+(function (num1, num2) {
+    console.log(num1 / num2);
+}
+)();
+// With parameter
+(function (num1, num2) {
+    console.log(num1 / num2);
+
+})(10, 5);
