babd3525f4ba97a4faa38e127056f4d9647609b9
CodeWithkaran02 <7870karuna@gmail.com>
2023-03-26 23:59:03 +0530

    path

---
+++ path.js
@@ -0,0 +1,7 @@
+let path = require("path");
+console.log(path);
+let extensionName = path.extname("P:\JS\os.js");
+console.log(extensionName);
+
+let basename = path.basename("string.js")
+console.log(basename);
