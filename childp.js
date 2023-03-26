c658225aa93ec2a261cd8c3c9cf345a7b40001c0
CodeWithkaran02 <7870karuna@gmail.com>
2023-03-26 23:51:27 +0530

    child_process

---
+++ childp.js
@@ -0,0 +1,10 @@
+let cp = require("child_process")
+let content = cp.execSync("node intro.js");
+console.log("Output of the file :" + content);
+console.log(cp);
+
+// buffer 
+/*let content = cp.execSync("node intro.js");
+console.log(content);
+console.log(cp);*/
+
