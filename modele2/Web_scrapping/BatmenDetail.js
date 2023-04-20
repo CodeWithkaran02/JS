52b82b3565c142e28cd35c2714191605a855549b
CodeWithkaran02 <7870karuna@gmail.com>
2023-04-20 22:56:00 +0530

    batsman

---
+++ modele2/Web_scrapping/BatmenDetail.js
@@ -0,0 +1,24 @@
+const request = require("request");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/royal-challengers-bangalore-vs-kolkata-knight-riders-eliminator-1254115/full-scorecard";
+
+request(link, cb);
+
+function cb(error, response, html) {
+    if (error) {
+        console.log(erroe);
+
+    }
+    else {
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let batsman = document.querySelectorAll(".batsman-cell.text-truncate.out a");
+        for (let i = 0; i < batsman; i++) {
+            let batmanLink = batsman[i].href;
+            let completeBatsmanLink = "https://www.espncricinfo.com" + batsmanLink;
+            console.log(completeBatsmanLink);
+        }
+    }
+}
