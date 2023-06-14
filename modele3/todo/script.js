75e96f24b4bc129b7c921466cec350139090311d
CodeWithkaran02 <7870karuna@gmail.com>
2023-06-14 22:11:03 +0530

    style in todo app

--- modele3/todo/script.js
+++ modele3/todo/script.js
@@ -1,30 +1,30 @@
-let inputTag = document.querySelector("input ");
+let inputTag = document.querySelector("input");
 let ulTag = document.querySelector("ul");
 
-inputTag.addEventListener('keyword', function () {
+inputTag.addEventListener('keydown', function (e) {
+    // console.log(e);
     let key = e.key;
-    if (key == Enter) {
+    if (key == 'Enter') {
         let value = inputTag.value;
-        //console.log(value );
-
+        // console.log(value);
         if (value.length == 0) {
             return;
         }
-        inputTag.value = " ";
+        inputTag.value = ""; //ui input update 
         let liTag = document.createElement("li");
-        liTag.innerHTML = `<div> ${value}</div>
-        <div class = "delelt"> <i class =" fa fa-trash"> </i> 
-        </div>`
+        liTag.innerHTML = `<div>${value}</div>
+                            <div class="delete"><i class="fa fa-trash"></i></div>`
 
+        //other way
+        // liTag.innerHTML = "<div>"+value+"</div><div><i class='fa fa-trash'></i></div>"
         handleRemoval(liTag);
         ulTag.appendChild(liTag);
-
     }
 })
-function handleRemoval(liTag) {
-    let deleltDiv = liTag.querySelector(".delelt");
 
-    deleltDiv.addEventListener("click ", function () {
+function handleRemoval(liTag) {
+    let deleteDiv = liTag.querySelector(".delete");
+    deleteDiv.addEventListener("click", function () {
         liTag.remove();
     })
 }
