b44fb1d151ea34a96a33b6651a7b9d0c6ca1cba9
CodeWithkaran02 <7870karuna@gmail.com>
2023-07-31 15:13:58 +0530

    style

--- modele3/excel clone/script.js
+++ modele3/excel clone/script.js
@@ -3,9 +3,9 @@ let cellcontentDiv = document.querySelector(".cell-contain")
 function initCells() {
     let cellconten = "<div class = 'top-left-cell'></div>";
     //top-row
-    cellconten += "<div class = 'top-row'>";
+    cellconten += "<div class = 'top-row'>"
     for (let i = 0; i < 26; i++) {
-        cellconten += `<div class = 'top-row-cell'>${String.fromCharCode(65 + i)}`;
+        cellconten += `<div class = 'top-row-cell'>${String.fromCharCode(65 + i)}</div>`;
     }
     cellconten += "</div>";
 
@@ -15,16 +15,17 @@ function initCells() {
         cellconten += `<div class = 'left-col-cell'>${i + 1}</div>`;
     }
     cellconten += "</div>";
-
+    cellconten += "<div class = 'cells'>"
     for (let i = 0; i < 100; i++) {
         cellconten += "<div class = 'row' >";
         //column
         for (let j = 0; j < 26; j++) {
-            cellconten += "<div class='cell'contentEditable></div>";
+            cellconten += "<div class='cell' contentEditable >cell</div>";
         }
 
         cellconten += "</div>";
     }
+    cellconten += "</div>"
     cellcontentDiv.innerHTML = cellconten;
 }
 initCells();
