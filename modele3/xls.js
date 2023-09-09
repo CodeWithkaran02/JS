71b03dd7107a56b057b5b10513511b49b574f1e5
CodeWithkaran02 <7870karuna@gmail.com>
2023-09-09 21:17:08 +0530

    file download

--- modele3/xls.js
+++ modele3/xls.js
@@ -23,4 +23,32 @@ document.getElementById('downloadExcelButton').addEventListener('click', () => {
     }, 100);
 });
 
+
+
+    <script>
+document.getElementById('downloadExcelButton').addEventListener('click', () => {
+    const table = document.getElementById('datatablesSimple');
+        const workbook = XLSX.utils.table_to_book(table);
+
+        // Convert the workbook to an array of arrays
+        const excelData = XLSX.write(workbook, {bookType: 'xlsx', type: 'array' });
+
+        // Convert the array to a Blob
+        const blob = new Blob([excelData], {
+            type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    });
+
+        // Create a Blob URL and trigger the download
+        const blobURL = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = blobURL;
+        link.download = 'survey_list.xlsx';
+        link.click();
+
+    setTimeout(() => {
+            URL.revokeObjectURL(blobURL);
+    }, 100);
+});
+
+    </script>
 </script>
